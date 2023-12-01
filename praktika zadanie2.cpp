00a20b067ae105493ccac0a665a711821eaefd68
Kirill Masalov <kirill.masa@yandex.ru>
2023-12-01 11:04:56 +0300

    fourth commit:r

--- praktika zadanie2.cpp
+++ praktika zadanie2.cpp
@@ -6,14 +6,14 @@ int main() {
 	int n;
 	cin >> n;
 	vector<int> vector1(n);
-	int max = 0;
+	int max = 100000000000;
 	cin.ignore();
 	for (int i = 0; i < n; i++) {
 		int x;
 		cin >> x;
 		vector1[i] = x;
-		if (vector1[i] > max) {
-			max = vector1[i];
+		if (vector1[i] < min) {
+			min = vector1[i];
 		}
 	}
 	cout << max << endl;
