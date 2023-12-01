0a089031e8ca854dde490dd1c31692e3ed131e36
Kirill Masalov <kirill.masa@yandex.ru>
2023-12-01 10:51:01 +0300

    fourth commit

--- praktika zadanie2.cpp
+++ praktika zadanie2.cpp
@@ -3,9 +3,15 @@
 using namespace std;
 
 int main() {
-	vector<int> vector1 = { 1, 2, 3, 4, 5, 6, 35, 12, 10};
+	int n;
+	cin >> n;
+	vector<int> vector1(n);
 	int max = 0;
-	for (int i = 0; i < vector1.size(); i++) {
+	cin.ignore();
+	for (int i = 0; i < n; i++) {
+		int x;
+		cin >> x;
+		vector1[i] = x;
 		if (vector1[i] > max) {
 			max = vector1[i];
 		}
