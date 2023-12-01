ecb5c1b2c88682110427227ec2bdd7781a0d89ff
Kirill Masalov <kirill.masa@yandex.ru>
2023-12-01 10:16:18 +0300

    first commit

---
+++ praktika zadanie2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<int> vector1 = { 1, 2, 3, 4, 5, 6, 35, 12, 10};
+	int max = 0;
+	for (int i = 0; i < vector1.size(); i++) {
+		if (vector1[i] > max) {
+			max = vector1[i];
+		}
+	}
+	cout << max << endl;
+	return 0;
+}
